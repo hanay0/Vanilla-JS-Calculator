9abbd7bc23a9f3ab177cff57832cd615d5a3b936
hanafy0 <30327222+hanay0@users.noreply.github.com>
2020-08-31 07:38:42 +0200

    handling numbers buttons

--- plugin.js
+++ plugin.js
@@ -12,8 +12,18 @@ let previousOperator;
 const screen = document.querySelector('.screen');
 
 function btnClick(value){
-    console.log(value);
+    isNaN(value) ? handleSymbols(value) : handleNumbers(value); 
 }
+
+// creating function to handle symobls and another one for numbers
+function handleSymbols(symbol){}
+function handleNumbers(numberString){// [numberString] refers to a number but it stills viewed as a string
+    if(buffer === '0') {
+      buffer = numberString;  
+    } else {
+        buffer += numberString;
+    }
+} 
 
 // creating init function which we will call it later to set up something
 function init(){
